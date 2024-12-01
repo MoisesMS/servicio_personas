696e8d555980b05c3753ccce4fdc0b2d53713b8c
Moises <moisesmorentes@gmail.com>
2024-12-01 20:42:40 +0100

    Ahora cada usuario solo recibe sus notas

--- routes/routes.js
+++ routes/routes.js
@@ -7,26 +7,44 @@ const router = express.Router()
 router.use(bodyParser.json())
 const notes = []
 router.post("/notes", async (req, res) => {
-  const token = req.header("Authorization").replace("Bearer ", "")
+  const token = getToken(req)
+  
+  let tokenValid = await isTokenValid(token)
 
-  try {
-    const response = await axios.post("http://localhost:3000/verifyToken", { token })
+  console.log(tokenValid)
+  if(tokenValid.valid) {
+    notes.push(req.body)
 
-    if(response.data.valid) {
-      const note = req.body
-      notes.push(note)
-
-      res.status(201).send("Nota creada")
-    } else {
-      res.status(401).send("Token inválido")
-    }
-  } catch(error) {
-    res.status(500).send("Authentication service is down")
+    res.status(201).send("Nota creada")
+  } else {
+    res.status(401).send("Token inválido")
   }
 })
 
-router.get("/notes", (req, res) => {
-  res.json(notes)
+router.get("/notes", async (req, res) => {
+  const token = getToken(req)
+  const tokenValid = await isTokenValid(token)
+  const userNotes = notes.filter(notes => notes.user === tokenValid.username)
+  if(tokenValid.valid) {
+    res.json(userNotes)
+  } else {
+    res.status(401).send("Token invalido")  }
 })
 
+const getToken = req => req.header("Authorization").replace("Bearer ", "")
+const isTokenValid = async token => {
+
+  try {
+    const response = await axios.post("http://localhost:3000/verifyToken", { token })
+    const resValid = response.data
+
+
+    return resValid
+  } catch(error) {
+    console.error(`Error en la verificación del token ${error.message}`)
+    return false
+  }
+
+  
+}
 module.exports = router
